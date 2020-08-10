64d832f63397f6ef19409a9664637af6a36c74a9
Efstathios Papakostas <csd4143@csd.uoc.gr>
2020-08-10 13:44:45 +0300

    Implemented command handler and recursive .js commandfiles lookup �

---
+++ src/commands/roll.js
@@ -0,0 +1,5 @@
+const { Message } = require('discord.js');
+
+module.exports.run = async (client, msg, args) => {
+	msg.channel.send('');
+};
--- src/main.js
+++ src/main.js
@@ -3,28 +3,56 @@ const PREFIX = process.env.PREFIX;
 const SECRET = process.env.SECRET;
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { promisify } = require('util');
-const readdir = promisify(require('fs').readdir);
+const fs = require('fs').promises;
+const path = require('path');
+
+client.commands = new Map();
 
 // Login with our discordsecret
 client.login(SECRET);
 
 // When we have inital handshake, log a message to the console
 client.on('ready', () => {
-	console.log(`Successfully connected as ${client.user.tag}`);
+	console.log(`Successfully connected as ${client.user.tag} ✅`);
 });
-
-const isValidCommand = (msg, cmdName) => {
-	msg.content.toLowerCase().startsWith(PREFIX + cmdName);
-};
 
 // Handle message
 client.on('message', (msg) => {
 	// Bot sending msg? Ignore
-	if (msg.author.bot) {
-		return;
-		// If not a bot, check if its our prefix
-	} else if (isValidCommand(msg)) {
-		msg.reply('gyrna sto terma sou paidaki');
+	// If not a bot, check if its our prefix
+	if (msg.author.bot || !msg.content.startsWith(PREFIX)) return;
+
+	// Split each piece without the prefix
+	let cmdMsg = msg.content.substring(msg.content.indexOf(PREFIX) + 1);
+	// Then Split to differect args
+	let cmdArgs = cmdMsg.split(new RegExp(/\s+/));
+	// Isolate command name
+	let cmdName = cmdArgs.shift();
+
+	// Lookup map, if command exists, run and pass client,msg,args instances
+	if (client.commands.get(cmdName)) {
+		client.commands.get(cmdName).run(client, msg, cmdArgs);
 	}
 });
+
+(async function registerCommands(dir = 'commands') {
+	// Read dir
+	let files = await fs.readdir(path.join(__dirname, dir));
+	for (let file of files) {
+		// Get n-th file
+		let stat = await fs.lstat(path.join(__dirname, dir, file));
+		// If directory treat it as 'dir/somefile' and traverse
+		if (stat.isDirectory()) {
+			registerModels(path.join(dir, file));
+		} else {
+			// If we found a file, check if it's *.js and register its name and module
+			// export to a Map
+			if (file.endsWith('.js')) {
+				let cmdName = file.substring(0, file.indexOf('.js'));
+				let cmdModule = require(path.join(__dirname, dir, file));
+				client.commands.set(cmdName, cmdModule);
+			}
+		}
+	}
+	console.log(`${client.commands.size} commands loaded ✅`);
+})();
