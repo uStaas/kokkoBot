a3964d2ed21e7648905b03952c10168649a4e6e7
Efstathios Papakostas <csd4143@csd.uoc.gr>
2020-08-17 14:17:38 +0300

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export let SECRET: string = 'your_token_here';
+export let PREFIX: string = '$';
+export let sound: string = 'some sound mp3 link';
+// Replace with however many reddits you like
+export let memeReddits: string[] = ['memes', 'dankmemes', 'meirl', 'me_irl', 'dankmeme', 'MemeEconomy'];
